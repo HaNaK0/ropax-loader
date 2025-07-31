e04e17edd2393aaef23d43f503ca3600aa2a2237
HaNa <hampus@hampushuledal.com>
2025-07-31 23:45:57 +0200

    update build termux build scriot to copy sprites add tugmaster sprite

--- build_love.sh
+++ build_love.sh
@@ -2,5 +2,6 @@
 #if [ -f game.love ]; then
 #	rm game.love
 #fi
+rsync -a ~/storage/pictures/Sprites/ropax_loader/ assets/sprites/
 zip -FS -r -i "*.lua" "assets/*" @ game.love . && \
 	cp -v game.love ~/storage/shared/Documents/konduktör/game.love 
---
+++ main.lua
@@ -0,0 +1,7 @@
+function love.load()
+end
+
+function love.draw()
+	love.graphics.setColor(1,1,1)
+	love.graphics.print("hello world", 100, 100)
+end
