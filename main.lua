1b01d77b07d2f5d032ecfed58dd09abbc6d5a1e7
HaNa <hampus@hampushuledal.com>
2025-08-02 04:50:39 +0200

    add conf and log
    add conf file for the game
    add the log system to main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,20 @@
+local log = require('log')
+
 function love.load()
+	log:setup()
+	love.physics.setMeter(64)
+	love.physics.newWorld(0, 0, true)
+
+	Objects = {}
+	Objects.ground = {}
+	
+end
+
+function love.update(dt)
+	log:update(dt)
+	Info(love.graphics.getWidth(), " ", love.graphics.getHeight())
 end
 
 function love.draw()
-	love.graphics.setColor(1,1,1)
-	love.graphics.print("hello world", 100, 100)
+	log:draw()
 end
