a8c3d104687e405568152c5198d228435e5f98cd
HaNaK0 <hampus@hampushuledal.com>
2026-03-31 14:31:40 +0200

    add vector drawing function
    add function to draw a vector as an arrow
    remove function for dot product to use the one built in to the vector

--- util/util.lua
+++ util/util.lua
@@ -9,12 +9,21 @@ function util.get_body_foreward(body)
 	return vector.fromAngle(body:getAngle())
 end
 
---- Take the dot product of two vectors
----@param vec_1 Vector.lua
----@param vec_2 Vector.lua
----@return number
-function util.dot_product(vec_1, vec_2)
-	return vec_1.x * vec_2.x + vec_1.y * vec_2.y
+--- get a vector that is perpendicular to the given vector
+---@param vec Vector.lua
+---@return Vector.lua
+function util.get_normal(vec)
+	return vector.new(-vec.y, vec.x)
+end
+
+--- Draw an arrow from the origin pointing in the direction of the vector
+---@param origin Vector.lua the origin for the arrow
+---@param vec Vector.lua the vector to draw
+function util.draw_vector(origin, vec)
+	local endpoint = origin + vec
+	local right_point = endpoint + vector.fromAngle(vec:heading() + 0.8 * math.pi) * 5
+	local left_piont = endpoint + vector.fromAngle(vec:heading() - 0.8 * math.pi) * 5
+	love.graphics.line(origin.x, origin.y, endpoint.x, endpoint.y, right_point.x, right_point.y, left_piont.x, left_piont.y, endpoint.x, endpoint.y)
 end
 
 return util
